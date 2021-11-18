ab9820a57c24ef128e440e1baa45f6c1375d160d
mlsmzk <71900463+mlsmzk@users.noreply.github.com>
2021-11-18 14:06:43 -0500

    Fix color masking, need to address error with cv2.rectangle

--- color_map_test_2.py
+++ color_map_test_2.py
@@ -1,44 +1,53 @@
 import cv2
 import numpy as np
-import math
+from math import sqrt
 from queue import Queue
 class MovingAvg:
 
     def __init__(self, max_vals = 10, threshold = .1):
-        self.values = Queue(max_vals)
-        self.avg = 0
+        self.values = []
+        self.max_vals = max_vals
+        self.avg = 0.0
         self.sum = 0
         self.threshold = threshold
         
     def push(self, val):
-        if self.values.full():
-            self.sum -= self.values.get()
+        if len(self.values) == self.max_vals:
+            if not self.withinBounds(val):
+                return
+            self.sum -= self.values.pop(0)
         
-        self.values.put(val)
+        self.values.append(val)
         self.sum += val
-        self.avg = self.sum / self.values.qsize()
+        if len(self.values) > 0:
+            self.avg = self.sum / len(self.values)
 
         return self.avg
 
-    def avg(self):
+    def average(self):
         return self.avg
     
     def full(self):
-        return self.values.full()
+        return len(self.values) == self.max_vals
 
     def withinBounds(self, val):
         return self.avg * (1 + self.threshold) >= val and self.avg * (1 - self.threshold) <= val
 
 # define a video capture object
 vid = cv2.VideoCapture(0)
-  
+
+
+mean_area = MovingAvg(10, .2)
+mean_center_x = MovingAvg(10, .2)
+mean_center_y = MovingAvg(10, .2)
+
 while(True):
       
     # Capture the video frame
     # by frame
     ret, img = vid.read()
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange (hsv, (0, 170, 100), (50, 255, 255))
+    mask = cv2.inRange (hsv, (0, 150, 100), (50, 255, 255))
     contours,_ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
@@ -52,27 +61,20 @@ while(True):
         (center_x,center_y) = ((2*x_min + box_width)/2, (2*y_min + box_height)/2)
         # print((center_x,center_y))
         
-        mean_area = MovingAvg(10, .01)
-        mean_center_x = MovingAvg(10, .01)
-        mean_center_y = MovingAvg(10, .01)
-        while not mean_area.full():
-            mean_area.push(box_area)
-        while not mean_center_x.full():
-            mean_center_x.push(center_x)
-        while not mean_center_y.full():
-            mean_center_y.push(center_y)
+        # while not mean_area.full():
+        mean_area.push(box_area)
+        # while not mean_center_x.full():
+        mean_center_x.push(center_x)
+        # while not mean_center_y.full():
+        mean_center_y.push(center_y)
         if mean_area.full() and mean_center_x.full() and mean_center_y.full():
-            print(mean_area.values.queue)
-            if mean_area.withinBounds(box_area):
-                if mean_center_x.withinBounds(center_x):
-                    if mean_center_y.withinBounds(center_y):
-                        mean_area.push(box_area)
-                        mean_center_x.push(center_x)
-                        mean_center_y.push(center_y)
-                        #drawing a rectangle around the object with 15 as margin
-                        cv2.rectangle(img, (x_min - 15, y_min - 15),
-                                    (x_min + box_width + 15, y_min + box_height + 15),
-                                    (0,255,0), 4)
+            side_length_half = sqrt(mean_area.average()) / 2
+            top_left = (mean_center_x.avg - side_length_half, mean_center_y.avg - side_length_half)
+            bottom_right = (mean_center_x.avg + side_length_half, mean_center_y.avg + side_length_half + 15)
+            #drawing a rectangle around the object with 15 as margin
+            cv2.rectangle(img, (x_min - 15, y_min - 15),
+                            (x_min + box_width + 15, y_min + box_height + 15),
+                            (0,255,0), 4)
 
     # Display the resulting frame
     cv2.imshow('frame', img)
