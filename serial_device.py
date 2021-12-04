c83e2384f9fbd8a4f1f0839a6b42256d37c13909
Abitamim <abitamimbharmal@hotmail.com>
2021-12-04 14:28:38 -0500

    /bin/python

--- serial_device.py
+++ serial_device.py
@@ -1,4 +1,5 @@
 # Jacob Smilg's serial_device code
+# Modified by Abitamim Bharmal
 
 import serial
 import serial.tools.list_ports as list_ports
@@ -17,12 +18,11 @@ class SerialDevice:
                     (0x2A03, 0x0043), (0x2341, 0x0243),
                     (0x0403, 0x6001), (0x1A86, 0x7523))
 
-    def __init__(self, port = None, baud = 115200) -> None:
+    def __init__(self, port = None, baud = 9600) -> None:
         self.ser = serial.Serial(timeout = 1)
         self.connected = False
         self.port = port
-        self.baud = self.confirm_baud(baud)
-        self.ser.baudrate = self.baud
+        self.ser.baudrate = baud
 
         # try to autoselect a port if no port was specified
         if not port:
@@ -32,12 +32,6 @@ class SerialDevice:
                     print('possible arduino port detected:')
                     print_port_info(port)
                     self.connect(port)
-            if not self.connected:
-                print('no recognized ports found. manually select one:')
-                
-                self.connect(self.manual_select_port())
-        else:
-            self.connect(port)
     
     def __del__(self) -> None:
         '''
@@ -59,88 +53,23 @@ class SerialDevice:
                 arduino_ports.append(port)
         return arduino_ports
 
-    def manual_select_port(self) -> ListPortInfo:
-        '''
-        have the user manually select a serial port to connect to
-        Returns:
-            ListPortInfo: the serial port chosen by the user.
-        '''
-        ports = list_ports.comports()
-        port_list = [port for port in ports]
-        print('found ports:')
-        for id, port in enumerate(port_list):
-            print('{}:'.format(id))
-            print_port_info(port)
-            print()
-
-        while True:
-            selection = input('select a port: ').strip()
-            if selection.isnumeric():
-                selection = int(selection)
-            if selection not in range(0, len(port_list)):
-                print('invalid selection!')
-            else:
-                return port_list[int(selection)]
-
     def connect(self, port: ListPortInfo) -> None:
         '''
         confirm connection to a serial port, and connect (or don't).
         Args:
             port (ListPortInfo): the port to possibly be connected to.
         '''
-        if yesno_confirm('connect with baud rate {}?'.format(self.baud)):
-            self.port = port
-            try:
-                self.ser.port = self.port.device
-                self.ser.baudrate = self.baud
-                self.ser.open()
-                self.connected = True
-                print('opened port {}'.format(port.name))
-            except:
-                print(('can\'t connect to port {}! is '+\
-                    'the port already in use?').format(self.port.device))
-                pass
-        else:
-            print('not connecting to port {}.'.format(port.name))
+        self.port = port
+        try:
+            self.ser.port = self.port.device
+            self.ser.open()
+            self.connected = True
+            print('opened port {}'.format(port.name))
+        except:
+            print(('can\'t connect to port {}! is '+\
+                'the port already in use?').format(self.port.device))
+            pass
 
-    def confirm_baud(self, baud: int) -> int:
-        '''
-        ask the user if they want to change the baud rate,
-        and change it to what they want.
-        Args:
-            baud (int): the currently selected baud rate.
-        Returns:
-            int: the user's choice of baud rate.
-        '''
-        print('the currently selected baud rate is {}.'.format(baud))
-        
-        if yesno_confirm('would you like to change it?'):
-            baud_choice = ''
-            while True:
-                baud_choice = input('enter a new baud rate: ').strip()
-                if not baud_choice.isnumeric():
-                    print('invalid input! please enter a number.')
-                else:
-                    baud_choice = int(baud_choice)
-                    if baud_choice in SerialDevice.EXTENDED_BAUDS:
-                        print(('baud rate {} is not standard, but may still '+\
-                                'be supported on some machines.').format(baud_choice))
-                        if yesno_confirm(('are you sure you want to select '+\
-                                'baud rate {}?').format(baud_choice)):
-                            print('using baud rate {}.'.format(baud_choice))
-                            return int(baud_choice)
-                    elif baud_choice in SerialDevice.STANDARD_BAUDS:
-                        print('using baud rate {}.'.format(baud_choice))
-                        return int(baud_choice)
-                    else:
-                        print(('baud rate {} is not standard, and is likely '+\
-                                'not supported.').format(baud_choice))
-                        if yesno_confirm(('are you sure you want to select '+\
-                                'baud rate {}?').format(baud_choice)):
-                            print('using baud rate {}.'.format(baud_choice))
-                            return int(baud_choice)
-        else:
-            return baud
 
     def read(self) -> str:
         '''
@@ -173,28 +102,3 @@ def print_port_info(port: ListPortInfo) -> None:
     print('\tdevice: {}'.format(port.device))
     print('\tdescription: {}'.format(port.description))
     print('\thwid: {}'.format(port.hwid))
-
-
-def yesno_confirm(message: str) -> bool:
-    '''
-    confirm a yes or no command line input option with the user.
-    Args:
-        message (str): the question to ask the user.
-    Returns:
-        bool: True if the user answers "y", False if not.
-    '''
-    confirm_choice = ''
-    while confirm_choice not in ('y', 'n'):
-        confirm_choice = input('{} (y/n) '.format(message)).lower().strip()
-        if confirm_choice not in ('y', 'n'):
-            print('please enter "y" or "n"')
-    return confirm_choice == 'y'
-
-
-if __name__ == '__main__':
-    # debug stuff, used to monitor the port when testing
-    dev = SerialDevice()
-    while True:
-        line = dev.read()
-        if len(line) > 0:
-            print(line, end='')
